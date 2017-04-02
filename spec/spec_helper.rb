44ba49918325b22370f027c5e40a65712ad321fe
John Skender <jnskender@gmail.com>
2017-04-01 21:18:29 -0500

    require collection_matchers, config fail_fast and color

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,12 @@
 require "bundler/setup"
 require "connect_four"
+require 'rspec/collection_matchers'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
+  config.fail_fast = true
+  config.color = true
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
