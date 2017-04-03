2e67d2501a2e63092e14defa62743319f58a100e
John Skender <jnskender@gmail.com>
2017-04-02 19:04:51 -0500

    update player information

--- lib/test_game.rb
+++ lib/test_game.rb
@@ -3,6 +3,6 @@ require "./connect_four.rb"
 
 puts "Welcome to Connect Four!"
 player1 = ConnectFour::Player.new({piece: "X", name: "Nick"})
-player2 = ConnectFour::Player.new({piece: "O", name: "John"})
+player2 = ConnectFour::Player.new({piece: "O", name: "Emily"})
 players = [player1,player2]
 ConnectFour::Game.new(players).play
